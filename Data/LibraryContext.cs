93a8d7091f516193bf1af32d7cf359255452eac0
Kleber Moraes <kleberksms@gmail.com>
2024-02-02 20:54:25 -0300

    feat: update to net 8

---
+++ Data/LibraryContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Book.Data;
+
+using Microsoft.EntityFrameworkCore;
+
+public class LibraryContext : DbContext
+{
+    public DbSet<Book> Books { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=LibraryDb;Trusted_Connection=True;");
+    }
+}
